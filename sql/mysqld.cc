b15cb4873fb6aa0410dc00243eb7afb40e7c81cd
Yoshinori Matsunobu <yoshinori@fb.com>
2016-02-29 09:22:53 -0800

    Optionally block/log queries relying on Gap Locks [non-RocksDB part]
    Summary:
    This diff adds three variables to control whether to
    raise errors/write logs when executing queries relying on Gap Locks.
    
    - gap_lock_raise_error (session, whether to raise error)
    - gap_lock_write_log (session, whether to write log)
    - gap_lock_log_file (global, log file name)
    
    Gap Lock log file is always created (default is $datadir/hostname-gaplock.log),
    but not written by default. Each session can control whether to write log or not.
    FLUSH GENERAL LOGS flushes Gap Lock log file as well as general log file.
    
    Queries relying on Gap Locks are determined by the following conditions.
    - Holding shared or exclusive locks
    - Not auto-committed, or using multiple tables (join/subquery, including
      self join)
    - Not using all primary key columns on WHERE clause (equal lookup)
    - Not executed from replication slaves
    If queries meet all conditions, they are queries relying on Gap Locks.
    
    This diff is for making migration from InnoDB to MyRocks easier.
    Intended use case is 1: setting gap_lock_raise_error=0,
    gap_lock_write_log=1 then listing up all queries relying on Gap Locks.
    2: Fixing all of them, or explicitly executing SET session
    gap_lock_raise_error=0 for queries that are absolutely safe. 3: setting
    gap_lock_raise_error=1 4: migrating from InnoDB to MyRocks.  These steps
    will make migration safer -- at least no queries relying on Gap Lock
    that MyRocks doesn't support will be executed.
    
    Differential Revision: https://reviews.facebook.net/D62139
    
    -------------------------------------------------------------------------------------
    
    Remove unnecessary DB_TYPE_LEVELDB and ASYNC_COMMIT
    
    Summary:
    DB_TYPE_LEVELDB is superceded by DB_TYPE_ROCKSDB and it was left over due to the way MyRocks was ported.
    ASYNC_COMMIT patch was dropped so I'm removing the related (commented) code as well.
    
    Reviewed By: lloyd
    
    Differential Revision: D19826886
    
    -------------------------------------------------------------------------------------
    
    fix QUERY_LOG_GAP_LOCK ER_CANT_OPEN_FILE log crashes the server
    
    Summary:
    in mysqld.cc `LogErr(ERROR_LEVEL, ER_CANT_OPEN_FILE)` crashes the server because it does not pass parameter for error message format.
    This log is not needed because `query_logger.reopen_log_file(QUERY_LOG_GAP_LOCK)` already writes error log on error.
    
    Differential Revision: D46373689
    
    Privacy Context Container: L1124648

--- sql/mysqld.cc
+++ sql/mysqld.cc
@@ -1498,6 +1498,7 @@ size_t mysql_data_home_len = 1;
 uint reg_ext_length;
 char logname_path[FN_REFLEN];
 char slow_logname_path[FN_REFLEN];
+char gap_lock_logname_path[FN_REFLEN];
 char secure_file_real_path[FN_REFLEN];
 Time_zone *default_tz;
 char *mysql_data_home = const_cast<char *>(".");
@@ -1677,6 +1678,7 @@ bool log_replica_updates_supplied = false;
 */
 bool replica_preserve_commit_order_supplied = false;
 char *opt_general_logname, *opt_slow_logname, *opt_bin_logname;
+char *opt_gap_lock_logname;
 
 /*
   True if expire_logs_days and binlog_expire_logs_seconds are set
@@ -5225,6 +5227,8 @@ int init_common_variables() {
               make_query_log_name(logname_path, QUERY_LOG_GENERAL));
   FIX_LOG_VAR(opt_slow_logname,
               make_query_log_name(slow_logname_path, QUERY_LOG_SLOW));
+  FIX_LOG_VAR(opt_gap_lock_logname,
+              make_query_log_name(gap_lock_logname_path, QUERY_LOG_GAP_LOCK));
 
 #if defined(ENABLED_DEBUG_SYNC)
   /* Initialize the debug sync facility. See debug_sync.cc. */
@@ -6843,6 +6847,10 @@ static int init_server_components() {
   query_logger.set_log_file(QUERY_LOG_GENERAL);
   if (opt_general_log && query_logger.reopen_log_file(QUERY_LOG_GENERAL))
     opt_general_log = false;
+
+  // Open gap lock log file
+  query_logger.set_log_file(QUERY_LOG_GAP_LOCK);
+  query_logger.reopen_log_file(QUERY_LOG_GAP_LOCK);
 
   /*
     Set the default storage engines
@@ -10179,6 +10187,7 @@ static int mysql_init_variables() {
   opt_skip_name_resolve = false;
   opt_general_logname = opt_binlog_index_name = opt_slow_logname = nullptr;
   opt_tc_log_file = "tc.log";  // no hostname in tc_log file name !
+  opt_gap_lock_logname = nullptr;
   opt_myisam_log = false;
   mqh_used = false;
   server_shutting_down = false;
@@ -12119,6 +12128,7 @@ PSI_file_key key_file_trn;
 PSI_file_key key_file_init;
 PSI_file_key key_file_general_log;
 PSI_file_key key_file_slow_log;
+PSI_file_key key_file_gap_lock_log;
 PSI_file_key key_file_relaylog;
 PSI_file_key key_file_relaylog_cache;
 PSI_file_key key_file_relaylog_index;
@@ -12152,6 +12162,7 @@ static PSI_file_info all_server_files[]=
   { &key_file_pid, "pid", 0, 0, PSI_DOCUMENT_ME},
   { &key_file_general_log, "query_log", 0, 0, PSI_DOCUMENT_ME},
   { &key_file_slow_log, "slow_log", 0, 0, PSI_DOCUMENT_ME},
+  { &key_file_gap_lock_log, "gap_lock_log", 0, 0, PSI_DOCUMENT_ME},
   { &key_file_tclog, "tclog", 0, 0, PSI_DOCUMENT_ME},
   { &key_file_trg, "trigger_name", 0, 0, PSI_DOCUMENT_ME},
   { &key_file_trn, "trigger", 0, 0, PSI_DOCUMENT_ME},
