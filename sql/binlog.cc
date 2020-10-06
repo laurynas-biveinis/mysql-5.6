bea106bbc63dff6020729ad3a9319225c3f94497
Luqun Lou <luqun@fb.com>
2020-10-06 16:50:14 -0700

    [raft] unlock channel_map for early return during register_entities_with_raft
    Summary: These early return need to unlock channel_map to avoid deadlock later.
    
    Differential Revision: D24151638

--- sql/binlog.cc
+++ sql/binlog.cc
@@ -9960,6 +9960,7 @@ static int register_entities_with_raft() {
         "Number of slave channels connected: %d. Cannot register "
         "with raft",
         channel_map.get_num_instances());
+    channel_map.unlock();
     return 1;  // error
   }
 
@@ -9967,6 +9968,7 @@ static int register_entities_with_raft() {
 
   if (!mi) {
     // degenerate case returns SUCCESS [ TODO ]
+    channel_map.unlock();
     return 0;
   }
 
@@ -9975,6 +9977,7 @@ static int register_entities_with_raft() {
   if (/*mi->host[0] || */ !mi->rli) {
     // degenerate case returns SUCCESS [ TODO ]
     mi->channel_unlock();
+    channel_map.unlock();
     return 0;
   }
 
--- sql/rpl_replica.cc
+++ sql/rpl_replica.cc
@@ -7803,6 +7803,7 @@ int heartbeat_queue_event(bool is_valid, Master_info *&mi,
 int update_rli_and_mi(
     const std::string &gtid_s,
     const std::pair<const std::string, unsigned long long> &master_log_pos) {
+  channel_map.rdlock();
   assert(channel_map.get_num_instances() == 1);
   Master_info *mi = channel_map.get_default_channel_mi();
   assert(mi != nullptr);
@@ -7823,17 +7824,24 @@ int update_rli_and_mi(
   // It is possible that this call was only done to update the master_log_pos
   // in which case an empty gtid would have been passed
   if (!gtid_s.length()) {
+    channel_map.unlock();
     return 0;
   }
 
+  mysql_mutex_t *log_lock = mi->rli->relay_log.get_log_lock();
+  mysql_mutex_assert_not_owner(log_lock);
+  mysql_mutex_lock(log_lock);
   const char *buf = gtid_s.c_str();
   Gtid_log_event gtid_ev(buf, mi->get_mi_description_event());
+  mysql_mutex_unlock(log_lock);
+
   Gtid gtid = {0, 0};
   rli->get_sid_lock()->rdlock();
   gtid.sidno = gtid_ev.get_sidno(rli->get_gtid_set()->get_sid_map());
   rli->get_sid_lock()->unlock();
   if (gtid.sidno < 0) {
     sql_print_information("could not get proper sid: %s", buf);
+    channel_map.unlock();
     return 1;
   }
 
@@ -7844,6 +7852,7 @@ int update_rli_and_mi(
   rli->get_sid_lock()->rdlock();
   rli->add_logged_gtid(gtid.sidno, gtid.gno);
   rli->get_sid_lock()->unlock();
+  channel_map.unlock();
 
   return 0;
 }
