9524570eff5e97fd95271458b1e912c13c8f8f26
Rahul Singhal <rahulsinghal@fb.com>
2021-03-18 14:08:54 -0700

    Porting gradual write throttling and configurable throttle dimensions logic
    Summary:
    This diff ports D26559698 (https://github.com/facebook/mysql-5.6/commit/bd3bd34c94b71f66ab393d5e8baf19cb2716b267) & D26217123 (https://github.com/facebook/mysql-5.6/commit/5eba6ba5ed11a807b75be2032b4e04e5154126e8) to 8.0
    
    [ D26217123 (https://github.com/facebook/mysql-5.6/commit/5eba6ba5ed11a807b75be2032b4e04e5154126e8) ]
    It adds gradual throttling logic to the existing replication lag auto throttling system.
    
    Note that, It does not affect manual throttling system. Any manual throttling rule will throttle 100% of the requests as soon as it is added.
    
    Added a new server variable `write-throttle-rate-step` which determines the step by which throttle rate is stepped up or down. It must be a number between 0-100. It defaults to 100 which means throttle rate is stepped up from 0% to 100% or it is stepped down from 100% to 0% which is the default behavior now.
    
    Added a new column to `I_S.write_throttling_rules` to display `throttle_rate`
    
    In addition, I also fixed a bug with clearing `auto_throttling_rules`, `currently_throttled_entities` & `currently_monitored_entity` when throttling rules are cleared or auto throttling is turned off. Previously, we were not clearing these data structures properly which would have caused some inconsistencies in the algorithm.
    
    [ D26559698 (https://github.com/facebook/mysql-5.6/commit/bd3bd34c94b71f66ab393d5e8baf19cb2716b267) ]
    
    It adds a server control to configure which type(dimensions -
    client/sql_id/user/shard) of entities are allowed to be throttled by the
    auto throttling system.
    
    This is a use case from TAO that they only would like to throttle one or
    more client_ids in case of replication lag. Similarly, for XDB we may
    want to throttle a sql_id and then a shard but never throttle a user id.
    
    This makes throttling logic more deterministic and easy to reason about.
    
    **This variable defines the dimensions that are okay to be throttled and
    the order in which auto throttling algorithm go through them to find a
    culprit.**
    
    ```
    DEFAULT - Do not throttle any dimension
    SET @GLOBAL.WRITE_THROTTLE_PERMISSIBLE_DIMENSIONS_IN_ORDER = “OFF”
    
    For UDB(TAO) - Only throttle client_id
    SET @GLOBAL.WRITE_THROTTLE_PERMISSIBLE_DIMENSIONS_IN_ORDER = “CLIENT”
    
    For some other hostname scheme (XDB) -
    SET @GLOBAL.WRITE_THROTTLE_PERMISSIBLE_DIMENSIONS_IN_ORDER = “SQL_ID,SHARD”
    ```
    
    Reviewed By: george-reynya
    
    Differential Revision: D27174135

--- storage/perfschema/pfs_dd_version.h
+++ storage/perfschema/pfs_dd_version.h
@@ -308,10 +308,13 @@
   - add SUM_FILESORT_DISK_USAGE to statements tables.
   - add SUM_TMP_TABLE_DISK_USAGE to statements tables.
 
+  80036-016:
+  - add throttle_rate column to write_throttling_rules table
+
   The last three digits reprents Facebook specific MySQL Schema changes.
-  Version published is now 80036-015. i.e. 8.0.36 Facebook schema change no. 15.
+  Version published is now 80036-016. i.e. 8.0.36 Facebook schema change no. 16.
 */
 
-static const uint PFS_DD_VERSION = 80036015;
+static const uint PFS_DD_VERSION = 80036016;
 
 #endif /* PFS_DD_VERSION_H */
