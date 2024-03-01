a63c9df6df4a84831e53afeafda37bbd91bd1af4
Alex Budovski <alexbud@meta.com>
2024-03-01 07:55:14 -0800

    mysql: size-based Parallel Table dump chunking
    Summary:
    This diff adds size-based chunking in addition to the row-count based
    one. Size-based allows us to have more uniformly-sized chunk files when
    there is high variance in row sizes in a table. This has been shown to
    be the case in prod, where the row-count based chunking has led to a
    very uneven distribution of chunk sizes.
    
    Differential Revision: D54440311
    
    fbshipit-source-id: 1a94e71f97a50df32101eeeb46e3d887237793be

--- sql/parser_yystype.h
+++ sql/parser_yystype.h
@@ -337,11 +337,26 @@ struct PT_install_component_set_element {
 };
 
 /**
+  Unit for chunking. Currently used by DUMP TABLE.
+*/
+enum class Chunk_unit {
+  UNSET,
+  ROWS,
+  KB,
+  MB,
+  GB,
+
+  // Add before this line.
+  LAST,
+};
+
+/**
   Options struct for PT_dump_table root node.
 */
 struct Dump_table_opts {
   static constexpr int DEFAULT_THREADS = 1;
   static constexpr int DEFAULT_CHUNKSIZE = 128;
+  static constexpr Chunk_unit DEFAULT_CHUNK_UNIT = Chunk_unit::ROWS;
 
   /**
     Number of worker threads to dump with.
@@ -349,9 +364,14 @@ struct Dump_table_opts {
   int nthreads;
 
   /**
-    Chunk size. For now in rows, later can be MB, GB, etc.
+    Chunk size, in units of `chunk_unit`.
   */
   int chunk_size;
+
+  /**
+    How to interpret `chunk_size`.
+  */
+  Chunk_unit chunk_unit;
 
   /**
     Whether to use a consistent snapshot / read.
@@ -368,6 +388,9 @@ struct Dump_table_opts {
     if (other.consistent) {
       consistent = true;
     }
+    if (other.chunk_unit != Chunk_unit::UNSET) {
+      chunk_unit = other.chunk_unit;
+    }
   }
 
   /**
@@ -377,6 +400,7 @@ struct Dump_table_opts {
     nthreads = 0;
     chunk_size = 0;
     consistent = false;
+    chunk_unit = Chunk_unit::UNSET;
   }
 
   /**
@@ -389,6 +413,9 @@ struct Dump_table_opts {
     }
     if (chunk_size == 0) {
       chunk_size = DEFAULT_CHUNKSIZE;
+    }
+    if (chunk_unit == Chunk_unit::UNSET) {
+      chunk_unit = DEFAULT_CHUNK_UNIT;
     }
   }
 };
@@ -489,6 +516,7 @@ union YYSTYPE {
   PT_order_list *order_list;
   Limit_options limit_options;
   Query_options select_options;
+  Chunk_unit chunk_unit;
   Dump_table_opts dump_table_opts;
   PT_limit_clause *limit_clause;
   Parse_tree_node *node;
